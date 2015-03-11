adf20da126303ef7a6b401af5b551323a201efcf
david viramontes <dviramontes@gmail.com>
2015-03-11 17:23:28 -0600

    adds return-component fn

--- src/cljs/sol_cavp/app.cljs
+++ src/cljs/sol_cavp/app.cljs
@@ -4,10 +4,9 @@
 
 (enable-console-print!) ;; enable print at web inspector console
 
-(def google-spreadsheet-url ;; READ-ONLY
-  "https://docs.google.com/spreadsheets/d/1SIN6GlkVozeZCe-MxjUXDV2fpnlWvKmKWaOdZmNbWGk/pubhtml")
+(def google-spreadsheet-url "https://docs.google.com/spreadsheets/d/1SIN6GlkVozeZCe-MxjUXDV2fpnlWvKmKWaOdZmNbWGk/pubhtml")
 
-(def master-form (atom nil))
+(def master-form (atom {}))
 
 (defn init-table-top []
   (let [enabled?  (fn [x] (when (= (:enabled x) "TRUE") x))
@@ -21,21 +20,25 @@
    [:div {:class "col-md-2 col-lg-2"} [:label label]]
    [:div {:class "col-md-4 col-lg-4"} body]])
 
-(defn text-input [label placeholder]
-  [row label 
-   [:input {:type "text" :class "form-control" :placeholder placeholder :size 20}]])
-
 (defn date-picker [setdate]
   (let [today (.format (js/moment (new js/Date)) "MM/DD/YYYY")]
-    (fn [] [:input#set-date {:type "text" :placeholder  "MM/DD/YYYY" :size 11 :value today}])))
+    (fn [] [:input#set-date {:type "text" :placeholder  "mm/dd/yyyy" :size 11 :value today}])))
 
 (defn date-picker-did-mount []
   (.ready (js/$ js/document) 
-          (fn [] (.datepicker (js/$ "#set-date") (clj->js {:format "MM/DD/YYYY"})))))
+          (fn [] (.datepicker (js/$ "#set-date") (clj->js {:format "mm/dd/yyyy"})))))
 
 (defn date-picker-component []
   (reagent/create-class {:render date-picker
                          :component-did-mount date-picker-did-mount}))
+(defn return-component [type]
+  (let [input-component (fn [x] [:input.form-control {                                                                       
+                                         :type x 
+                                         :on-change #(print (-> % .-target .-value))}])]
+    (case type
+      "datepicker" [date-picker-component]
+      [input-component type])))
+
 (defn part-1 []
   (let []
     (fn []
@@ -45,11 +48,9 @@
        [:h2.text-right "part 1"]]
       [:br]
       (for [c @master-form]
-        ^{:key c} [row (:field-name c) [:input.form-control {:key (:rowNumber c) 
-                                                             :type (:type c) 
-                                                             ;; (-> % .-target .-value)
-                                                             :on-change #(print (-> % .-target .-value))}]])])))
-
+        ^{:key c} [row (:field-name c) [:div {:key (str "part-1-" (:rowNumber c))}                                       
+                                        [return-component (:type c)]]])]))) 
+                                                                 
 (defn init []
   (init-table-top)
   (reagent/render-component [part-1] (.getElementById js/document "mount")))
