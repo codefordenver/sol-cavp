b0c558007f53de13f43fab295b88660fb730c612
david viramontes <dviramontes@gmail.com>
2015-03-14 22:00:38 -0600

    adds login-btn fn

--- src/cljs/sol_cavp/app.cljs
+++ src/cljs/sol_cavp/app.cljs
@@ -10,7 +10,7 @@
 
 (def lock (new js/Auth0Lock "6I7t0JdYGGTFoxeB8gmW0LWn7LciMm7G" "sol-cavp.auth0.com")) ;; auth0 lock 
 
-(def user-profile (atom {})) ;; profile
+(def user (atom {:logged-in false :profile {}})) ;; profile
 
 (defn init-table-top []
   (let [enabled?  (fn [x] (when (= (:enabled x) "TRUE") x))
@@ -35,10 +35,11 @@
 (defn date-picker-component []
   (reagent/create-class {:render date-picker
                          :component-did-mount date-picker-did-mount}))
+
 (defn return-component [type]
-  (let [input-component (fn [x] [:input.form-control {                                                                       
-                                         :type x 
-                                         :on-change #(print (-> % .-target .-value))}])]
+  (let [input-component (fn [x] [:input.form-control {        
+                                                      :type x 
+                                                      :on-change #(print (-> % .-target .-value))}])]
     (case type
       "datepicker" [date-picker-component]
       [input-component type])))
@@ -48,39 +49,50 @@
    [:p.text-center "notes"]
    [:textarea.form-control {:rows 24}]])
 
-(defn login []
-  (.show lock (clj->js {:popup true}) 
-         (fn [err profile token]
-           (if (not (nil? err))
-             (print err)
-             (do 
-               (.setItem js/localStorage (clj->js {:userToken token})) ;; save the JWT token.
-               (swap! user-profile profile)
-               (print profile)
-               (print token))))))
-
 (defn login-btn []
-  [:label {:for "btn-login"} "Are you a volunteer? "
-   [:button {:class "btn btn-default" :id "btn-login" :type "submit" :on-click login} "login"]])
+
+  (let [state (atom {:auth false})
+        logged-in? #(if (false? (:auth @state)) "login" "logout")
+        in-out (fn [] (if (false? (:auth @state))
+                              (.show lock (clj->js {:popup true}) 
+                                     (fn [err profile token]
+                                       (if (not (nil? err))
+                                         (print err)
+                                         (do 
+                                           (.setItem js/localStorage "userToken" token) ;; save the JWT token.
+                                           ;; (swap! assoc :profile (js->clj profile :keywordize-keys true))
+                                           (swap! state assoc :auth true)
+                                           (print profile)
+                                           (print token)))))
+                              (do 
+                               (.removeItem js/localStorage "userToken")
+                               ;; (aset js/document "location" "href" "/")
+                               (swap! state assoc :auth false))))] 
+
+    (fn []      
+      (print (str "user logged in :: " @state))
+      [:label {:for "btn-login"} "Are you a volunteer? "
+       [:button {:class "btn btn-default" :id "btn-login" :type "submit" :on-click in-out}
+        (logged-in?)]])))
 
 (defn part-1 []
   (let []
     (fn []
       (println (count @master-form))     
       [:div 
-         [:div.header]
+       [:div.header]
        [:h5.text-right
         [login-btn]
         [:br]
         "part " [:span.badge "1"]]          
-      [:br]
-      [:div.row
-       [:div {:class "col-md-6 col-lg-6"} 
-        (for [c @master-form]
-          ^{:key c} [row (:field-name c) [:div {:key (str "part-1-" (:rowNumber c))}                                       
-                                          [return-component (:type c)]]])]
+       [:br]
+       [:div.row
+        [:div {:class "col-md-6 col-lg-6"} 
+         (for [c @master-form]
+           ^{:key c} [row (:field-name c) [:div {:key (str "part-1-" (:rowNumber c))} 
+                                           [return-component (:type c)]]])]
         [text-area]]]))) 
-                                                                 
+
 (defn init []
   (init-table-top)
   (reagent/render-component [part-1] (.getElementById js/document "mount")))
